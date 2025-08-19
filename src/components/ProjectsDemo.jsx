9b186f80a45f7bb13e53a784913a5dbb86545afe
yacine namane <yacine.nam@outlook.fr>
2025-08-19 18:47:09 +0200

    Avancement sur le composant ProjectDemo avec GSAP , update Structure du cote et css du composant

--- src/components/ProjectsDemo.jsx
+++ src/components/ProjectsDemo.jsx
@@ -7,7 +7,6 @@ import UD3 from "../images/UD3.png";
 import UD4 from "../images/UD4.png";
 import UD5 from "../images/UD5.png";
 import UD6 from "../images/UD6.png";
-import PBG from "../images/PBG.gif";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,78 +15,68 @@ const ProjectsDemo = () => {
     const slides = document.querySelectorAll(".slide");
 
     slides.forEach((slide) => {
-      // Animation du texte
-      gsap.fromTo(
-        slide.querySelector(".text"),
-        { opacity: 0, y: 60 },
+      const images = slide.querySelectorAll(".images img");
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: slide,
+          start: "top 100%",
+          end: "bottom 20%",
+          scrub: true,
+        },
+      });
+
+      tl.fromTo(
+        images,
+        { opacity: 0, y: "100vh" },
         {
           opacity: 1,
           y: 0,
+          stagger: 0.3,
+          ease: "power3.out",
           duration: 1,
-          scrollTrigger: {
-            trigger: slide,
-            start: "top 80%",
-            end: "bottom 20%",
-            scrub: 1,
-          },
         }
       );
 
-      // Animation des images
-      gsap.fromTo(
-        slide.querySelectorAll(".images img"),
-        { opacity: 0, y: 50, scale: 0.95 },
-        {
-          opacity: 1,
-          y: 0,
-          scale: 1,
-          stagger: 0.2,
-          duration: 1,
-          scrollTrigger: {
-            trigger: slide,
-            start: "top 80%",
-            end: "bottom 20%",
-            scrub: 1,
-          },
-        }
-      );
+      tl.to(images, {
+        opacity: 0,
+        y: "-100vh",
+        stagger: 0.3,
+        ease: "power1.in",
+        duration: 1,
+      });
     });
   }, []);
 
   return (
-    <div
-      className="demo-container"
-      style={{ position: "relative", overflow: "hidden" }}
-    >
-      {/* Background GIF */}
+    <div className="demo-container" style={{ position: "relative" }}>
       <img
-        src={PBG}
-        alt="background gif"
         className="background-gif"
+        alt="bg management"
         style={{
-          position: "absolute",
+          position: "fixed",
+          background: "#141313",
           top: 0,
           left: 0,
           width: "100%",
-          height: "100%",
+          height: "100vh",
           objectFit: "cover",
           zIndex: -1,
         }}
       />
 
-      {/* Slide 1 */}
       <div
         className="slide"
-        style={{ padding: "100px 0", textAlign: "center" }}
+        style={{ padding: "120px 0", textAlign: "center" }}
       >
-        <div className="text animate">
+        <div className="text">
           <h2>UtopiaDreams</h2>
           <p>
             Une plateforme immersive pour découvrir des fonds d'écran uniques.
           </p>
         </div>
         <div
-          className="images animate"
+          className="images"
           style={{
             display: "flex",
             justifyContent: "center",
@@ -95,35 +84,22 @@ const ProjectsDemo = () => {
             marginTop: "40px",
           }}
         >
-          <img
-            src={UD1}
-            alt="UD 1"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
-          <img
-            src={UD2}
-            alt="UD 2"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
-          <img
-            src={UD3}
-            alt="UD 3"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
+          <img src={UD4} alt="UD 4" />
+          <img src={UD2} alt="UD 2" />
+          <img src={UD3} alt="UD 3" />
         </div>
       </div>
 
-      {/* Slide 2 */}
       <div
         className="slide"
-        style={{ padding: "100px 0", textAlign: "center" }}
+        style={{ padding: "120px 0", textAlign: "center" }}
       >
-        <div className="text animate">
+        <div className="text">
           <h2>Fonctionnalités</h2>
           <p>Explorez une variété de fonds d'écran classés par catégories.</p>
         </div>
         <div
-          className="images animate"
+          className="images"
           style={{
             display: "flex",
             justifyContent: "center",
@@ -131,21 +107,9 @@ const ProjectsDemo = () => {
             marginTop: "40px",
           }}
         >
-          <img
-            src={UD4}
-            alt="UD 4"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
-          <img
-            src={UD5}
-            alt="UD 5"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
-          <img
-            src={UD6}
-            alt="UD 6"
-            style={{ width: "200px", borderRadius: "12px" }}
-          />
+          <img src={UD5} alt="UD 5" />
+          <img src={UD1} alt="UD 1" />
+          <img src={UD6} alt="UD 6" />
         </div>
       </div>
     </div>
